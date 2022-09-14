0f4d555a06fa7d6dba90300e35d9884d0b4b9dd9
bendesousa <bdsousa069@gmail.com>
2022-09-14 10:11:30 +0100

    added get games mapping

--- backend_project/src/main/java/com/example/backend_project/controllers/GameController.java
+++ backend_project/src/main/java/com/example/backend_project/controllers/GameController.java
@@ -1,6 +1,7 @@
 package com.example.backend_project.controllers;
 
 import com.example.backend_project.models.Battle;
+import com.example.backend_project.models.Game;
 import com.example.backend_project.models.Player;
 import com.example.backend_project.models.Reply;
 import com.example.backend_project.services.BattleService;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -34,11 +36,20 @@ public class GameController {
     PlayerService playerService;
 
     @GetMapping
+    public ResponseEntity<List<Game>> getAllGames(){
+        List<Game> games;
+        games = gameService.getGames();
+        return new ResponseEntity<>(games, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/combat")
     public ResponseEntity<Reply> battleOutcome(@RequestParam long playerId,@RequestParam long battleId){
         Player player = playerService.getPlayerById(playerId).get();
         Battle battle = battleService.getBattleById(battleId).get();
         return new ResponseEntity<>(battleService.combatOutcome(player,battle), HttpStatus.OK);
     }
+
+    @
 
 
 }
--- backend_project/src/main/java/com/example/backend_project/models/Game.java
+++ backend_project/src/main/java/com/example/backend_project/models/Game.java
@@ -1,11 +1,21 @@
 package com.example.backend_project.models;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity(name = "games")
 public class Game {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "player_id")
+    @JsonIgnoreProperties({"games"})
     private Player player;
     private List<Battle> battles;
 
